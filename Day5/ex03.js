d1991eb38f291fe681cb38e77b2dacdb78d114f7
mstars_lab2_16 <mstarsacademy@Mstarss-iMac-2.local>
2022-04-20 14:19:32 +0800

    Day5 exercise

---
+++ Day5/ex03.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+var http = require("http");
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(200);
+    const data = fs.readFileSync("ex03.html");
+    response.end(data);
+  })
+  .listen(3000);
+
+console.log("Server is started");
